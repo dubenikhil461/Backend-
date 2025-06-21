66cac3e619fc4604402f48109f878c925c94fc1b
Nikhil Dubey <nikhildubey461@gmail.com>
2025-06-21 18:55:04 +0530

    Enhance error handling: add 404 response for undefined routes and improve logging for the PORT variable

--- app.js
+++ app.js
@@ -7,13 +7,20 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // For JSON payloads
 app.use(express.urlencoded({ extended: true })); // For form datadb();
-// ✅ Routes
+
 app.get('/', (req, res) => {
   res.send('api is working...');
 });
  db()
 app.use('/p/user',User)
-// ✅ Fix PORT variable name in log
+
+app.all('*',(req,res,next)=>{
+  res.status(404).json({
+    status : 'failed',
+    message : `can't find ${req.originalUrl} on this server`
+  });
+  // next()
+})
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port http://localhost:${process.env.PORT}`);
 });
